a7773c3200cfc445512ac2466d21bfd1c6c2fda4
phamducfpt <ducpmth2009023@fpt.edu.vn>
2021-06-30 10:09:02 +0700

    update

--- ThiAP/Program.cs
+++ ThiAP/Program.cs
@@ -15,6 +15,26 @@ namespace ThiAP
                 Console.WriteLine("2.hiển thị sản phẩm");
                 Console.WriteLine("3.xóa sản phẩm");
                 Console.WriteLine("4.thoát");
+                Console.Write("Nhap tuy chon: ");
+                int key = Convert.ToInt32(Console.ReadLine());
+                switch (key)
+                {
+                    case 1:
+                        Console.WriteLine("\n1.thêm sản phẩm.");
+                        quanLySp.NhapSanPham();
+                        Console.WriteLine("\nThêm thông tin thành công");
+                        break;
+                    case 2:
+                        int id;
+                            Console.WriteLine("\n3. Xóa sản phẩm.");
+                            Console.Write("\nNhập ID: ");
+                            id = Convert.ToInt32(Console.ReadLine());
+                        if (quanLySp.DeleteById(id))
+                        {
+                            Console.WriteLine("\n Xóa thành công");
+                        }
+
+                        break;
             }
         }
     }
